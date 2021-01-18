cd4f8357ebfb1d0b846c2e7cfbf0149b31b8222f
Sumanth Mallya <sumanth.89@gmail.com>
2021-01-18 11:58:18 -0600

    Added file rename with date

--- Start.sh
+++ Start.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+now=$(date)
+
+
 python3 CDC_Scrapper.py
+
+sleep 5
+
+if ls ${PWD}/Data/covid19_vaccinations_in_the_united_states.csv; then
+    mv "${PWD}/Data/covid19_vaccinations_in_the_united_states.csv" "$PWD/Data/$now"
+fi
+
+
